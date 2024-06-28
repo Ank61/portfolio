8a56c77036e19ba07210bd323b3f4a06cbd055ef
Ankit <ankitchohan61@gmail.com>
2024-06-28 18:59:19 +0530

    added routers

--- components/navbar.js
+++ components/navbar.js
@@ -1,22 +1,25 @@
 "use client"
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 export default function Navbar() {
+    const pathname = usePathname()
+
     return (
         <div className="flex items-center flex-wrap gap-2 mb-8">
             <Link href="/">
-                <button className="text-white text-sm transition-colors px-2 py-1 rounded-md relative"><span className="relative z-10">About</span><span className="absolute inset-0 z-0 bg-gray-900 rounded-md" style={{ opacity: 1 }}></span></button>
+                <button className={`${pathname==="/" ? "text-white ":"text-black"} text-sm transition-colors px-2 py-1 rounded-md relative`}><span className="relative z-10">About</span><span className={`${pathname==="/" ? "bg-gray-900" : "bg-white"} absolute inset-0 z-0 bg-gray-900 rounded-md`}></span></button>
             </Link>
             <Link href="/blogs">
-                <button className="text-gray-500 hover:text-gray-900 text-sm transition-colors px-2 py-1 rounded-md relative"><span className="relative z-10">Blogs</span></button>
+            <button className={`${pathname==="/blogs" ? "text-white ":"text-black"} text-sm transition-colors px-2 py-1 rounded-md relative`}><span className="relative z-10">Blogs</span><span className={`${pathname==="/" ? "bg-gray-900" : "bg-white"} absolute inset-0 z-0 bg-gray-900 rounded-md`}></span></button>
             </Link>
             <Link href="/uses">
-                <button className="text-gray-500 hover:text-gray-900 text-sm transition-colors px-2 py-1 rounded-md relative"><span className="relative z-10">Uses</span></button>
+            <button className={`${pathname==="/uses" ? "text-white ":"text-black"} text-sm transition-colors px-2 py-1 rounded-md relative`}><span className="relative z-10">Uses</span><span className={`${pathname==="/" ? "bg-gray-900" : "bg-white"} absolute inset-0 z-0 bg-gray-900 rounded-md`}></span></button>
             </Link>
             <Link href="/newsletter">
-                <button className="text-gray-500 hover:text-gray-900 text-sm transition-colors px-2 py-1 rounded-md relative"><span className="relative z-10">Newsletter</span></button>
+            <button className={`${pathname==="/newsletter" ? "text-white ":"text-black"} text-sm transition-colors px-2 py-1 rounded-md relative`}><span className="relative z-10">Newsletter</span><span className={`${pathname==="/" ? "bg-gray-900" : "bg-white"} absolute inset-0 z-0 bg-gray-900 rounded-md`}></span></button>
             </Link>
             <Link href="/contact">
-                <button className="text-gray-500 hover:text-gray-900 text-sm transition-colors px-2 py-1 rounded-md relative"><span className="relative z-10">Contact</span></button>
+            <button className={`${pathname==="/contact" ? "text-white ":"text-black"} text-sm transition-colors px-2 py-1 rounded-md relative`}><span className="relative z-10">Contact</span><span className={`${pathname==="/" ? "bg-gray-900" : "bg-white"} absolute inset-0 z-0 bg-gray-900 rounded-md`}></span></button>
             </Link>
         </div>
     )
